49ea2e82bb6a20682f1752910e4722dbe846a22c
Ryan <150871326+DangDacTu@users.noreply.github.com>
2026-02-20 20:24:11 +0700

    Update 2sum.py

--- 2sum.py
+++ 2sum.py
@@ -3,7 +3,7 @@ class Solution(object):
     def twoSum(self, nums, target):
         seen = {} 
         for i, num in enumerate(nums):
-            complement = target - num  # Tính giá trị còn thiếu để đạt target
+            complement = target - num 
             if complement in seen:  # Nếu giá trị còn thiếu đã xuất hiện
                 return [seen[complement], i]  # Trả về cặp chỉ số
             seen[num] = i  # Lưu giá trị hiện tại vào dictionary
